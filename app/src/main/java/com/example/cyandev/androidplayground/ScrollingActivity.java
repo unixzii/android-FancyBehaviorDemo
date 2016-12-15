f05d4983b511953f0805cce498f329f53f840955
杨弘宇 <unixzii@gmail.com>
2016-12-15 11:05:36 +0800

    initial commit

---
+++ app/src/main/java/com/example/cyandev/androidplayground/ScrollingActivity.java
@@ -0,0 +1,56 @@
+package com.example.cyandev.androidplayground;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                return new ViewHolder(getLayoutInflater().inflate(R.layout.item_simple, parent, false));
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                ViewHolder vh = (ViewHolder) holder;
+                vh.text.setText("Fake Item " + (position + 1));
+                vh.text2.setText("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+
+            class ViewHolder extends RecyclerView.ViewHolder {
+
+                TextView text;
+                TextView text2;
+
+                public ViewHolder(View itemView) {
+                    super(itemView);
+
+                    text = (TextView) itemView.findViewById(R.id.text);
+                    text2 = (TextView) itemView.findViewById(R.id.text2);
+                }
+
+            }
+        });
+    }
+}
